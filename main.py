f522f07e40b9d62a39f5a836e35fc71b1822fe8f
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-04-02 22:19:53 +0200

    #1 - redesign using functions

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Entry point
-print("Welcome to LogConverter app")
-print("Please enter log location using \\ like separator ")
-logPath = input("Enter file path here: ")
-print("You enetered " + logPath)
+def welcome():
+    print("Welcome to LogConverter app")
+
+
+def get_log_path():
+    print("Please enter log location using \\ like separator ")
+    file_path = input("Enter file path here: ")
+    print("You enetered " + file_path)
+    return file_path
+
+
+welcome()
+log_path = get_log_path()
