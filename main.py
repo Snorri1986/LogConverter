3b2705e62b1d655e7c36d6ef7665fece734ef36a
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-04-03 20:06:14 +0200

    #1 - enough for init

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import unittest
 
+
 # Entry point
 def welcome():
     print("Welcome to LogConverter app")
-
-
-#TODO: add check by NULL
-def get_log_path():
-    print("Please enter log location using \\ like separator ")
-    file_path = input("Enter file path here: ")
-    print("You enetered " + file_path)
-    return file_path
+    print("Version: 1.0.0")
 
 
 welcome()
-log_path = get_log_path()
-
-#TODO: finish unit test
-class Testing(unittest.TestCase):
-    def test_get_log_path(self):
-        test_log_path=get_log_path()
-
