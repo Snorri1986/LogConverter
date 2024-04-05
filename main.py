7cdf73f270404267677c477cf08586bf51a2946f
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-04-05 20:36:10 +0200

    #4 - got file more correct name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Entry point
-import file_path_initializer
+import prep
 
 
 def welcome():
     print("Welcome to LogConverter app")
     print("Version: 1.0.0")
-    file_path_initializer.init_path_variables()
+    prep.init_path_variables()
 
 
 welcome()
--- test_file_path_initializer.py
+++ test_file_path_initializer.py
@@ -1,12 +1,12 @@
 import unittest
-import file_path_initializer
+import prep
 
 
 class MyTestCase(unittest.TestCase):
-    def test_init_path_variables(self):
-        file_path_initializer.init_path_variables()
-        self.assertIsNotNone(file_path_initializer.deposit_task_log_path)
-        self.assertIsNotNone(file_path_initializer.loan_task_log_path)
+    def test_prep(self):
+        prep.init_path_variables()
+        self.assertIsNotNone(prep.deposit_task_log_path)
+        self.assertIsNotNone(prep.loan_task_log_path)
 
 
 if __name__ == '__main__':
