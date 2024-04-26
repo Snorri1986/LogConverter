b6c5ef7d7960a65814396b010c59133e9c599d79
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-04-26 20:33:28 +0200

    #6 - funcs for reading txt logs(final v1)

--- reading.py
+++ reading.py
@@ -1,6 +1,7 @@
 # file contains functions for reading logs from txt
 
 def read_deposit_log(latest_deposit_file_name,path_to_directory):
+    # full path to file
     path_to_file = path_to_directory + '\\' + latest_deposit_file_name
     deposit_log_txt = open(path_to_file, 'r')
     count = 0
@@ -12,6 +13,7 @@ def read_deposit_log(latest_deposit_file_name,path_to_directory):
     deposit_log_txt.close()
 
 def read_loans_log(latest_loans_file_name,path_to_directory):
+    # full path to file
     path_to_file = path_to_directory + '\\' + latest_loans_file_name
     loans_log_txt = open(path_to_file, 'r')
     count = 0
