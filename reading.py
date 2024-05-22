e4effc6f8e6e6c8fc2237d05effed8eaa29aa49c
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-05-22 20:00:07 +0200

    #6 - funcs for reading txt logs(final)

--- reading.py
+++ reading.py
@@ -1,25 +1,28 @@
 # file contains functions for reading logs from txt
 
 def read_deposit_log(latest_deposit_file_name,path_to_directory):
+    deposit_log_lines = []
     # full path to file
     path_to_file = path_to_directory + '\\' + latest_deposit_file_name
     deposit_log_txt = open(path_to_file, 'r')
     count = 0
 
     for line in deposit_log_txt:
+        deposit_log_lines = [line.strip() for line in deposit_log_txt]
         count += 1
-        print("Line{}: {}".format(count, line.strip()))
 
     deposit_log_txt.close()
 
+
 def read_loans_log(latest_loans_file_name,path_to_directory):
+    loans_log_lines = []
     # full path to file
     path_to_file = path_to_directory + '\\' + latest_loans_file_name
     loans_log_txt = open(path_to_file, 'r')
     count = 0
 
     for line in loans_log_txt:
+        loans_log_lines = [line.strip() for line in loans_log_txt]
         count += 1
-        print("Line{}: {}".format(count, line.strip()))
 
     loans_log_txt.close()
